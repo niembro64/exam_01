f1589716a427da86809d80b8b64cb183ea97df42
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-22 12:28:04 -0400

    session

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,27 +23,116 @@ namespace exam_01.Controllers
       _logger = logger;
       _context = context;
     }
-
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public IActionResult Index()
     {
       return Redirect("/login");
     }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     [HttpGet("/login")]
     public IActionResult Login()
     {
+      ViewBag.AllUsers = _context.Users.OrderBy(a => a.Name).ToList();
+      ViewBag.AllMeetups = _context.Meetups.OrderBy(a => a.Title).ToList();
+      ViewBag.Session_UserId = HttpContext.Session.GetInt32("Session_UserId");
+      ViewBag.Session_Name = HttpContext.Session.GetString("Session_Name");
       return View();
     }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    [HttpGet("/logout")]
+    public IActionResult LogOut()
+    {
+      // need to clear session
+      HttpContext.Session.Clear();
+
+      return Redirect("/login");
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     [HttpGet("/meetups")]
     public IActionResult Meetups()
     {
+      ViewBag.AllUsers = _context.Users.OrderBy(a => a.Name).ToList();
+      ViewBag.AllMeetups = _context.Meetups.OrderBy(a => a.Title).ToList();
+      ViewBag.Session_UserId = HttpContext.Session.GetInt32("Session_UserId");
+      ViewBag.Session_Name = HttpContext.Session.GetString("Session_Name");
       return View();
     }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     [HttpGet("/new")]
     public IActionResult NewMeetup()
     {
+      ViewBag.AllUsers = _context.Users.OrderBy(a => a.Name).ToList();
+      ViewBag.AllMeetups = _context.Meetups.OrderBy(a => a.Title).ToList();
+      ViewBag.Session_UserId = HttpContext.Session.GetInt32("Session_UserId");
+      ViewBag.Session_Name = HttpContext.Session.GetString("Session_Name");
       return View();
     }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    [HttpPost("users/login")]
+    public IActionResult LogUser(LoginUser loggedIn)
+    {
+      Console.WriteLine("--------------------------LogUser");
+      if (ModelState.IsValid)
+      {
+        Console.WriteLine("--------------------------ModelStateIsValid");
+        User userInDb = _context.Users.FirstOrDefault(a => a.Email == loggedIn.LoginEmail);
+        if (userInDb == null)
+        {
+          Console.WriteLine("--------------------------== null");
+          ModelState.AddModelError("LoginEmail", "Invalid login attempt");
+          return View("Login");
+        }
+        Console.WriteLine("--------------------------Hashing Passwords");
+        var hasher = new PasswordHasher<LoginUser>();
+        var result = hasher.VerifyHashedPassword(loggedIn, userInDb.Password, loggedIn.LoginPassword);
+        if (result == 0)
+        {
+          Console.WriteLine("--------------------------result == 0");
+          ModelState.AddModelError("LoginEmail", "Invalid login attempt");
+          return View("Login");
+        }
+        Console.WriteLine("--------------------------Redirecting to Meetups");
 
+        HttpContext.Session.SetString("Session_Name", userInDb.Name);
+        HttpContext.Session.SetInt32("Session_UserId", userInDb.UserId);
+        return RedirectToAction("Meetups");
+      }
+      Console.WriteLine("--------------------------ModelState Invalid");
+
+      return View("Login");
+
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    [HttpPost("users/add")]
+    public IActionResult AddUser(User newUser)
+    {
+      if (ModelState.IsValid)
+      {
+
+        // Duplicate Emails
+        if (_context.Users.Any(s => s.Email == newUser.Email))
+        {
+          ModelState.AddModelError("Email", "This email already in use.");
+          return View("Login");
+        }
+
+        // Hash Pass
+        PasswordHasher<User> Hasher = new PasswordHasher<User>();
+        newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+
+        _context.Users.Add(newUser);
+        _context.SaveChanges();
+
+        HttpContext.Session.SetString("Session_Name", newUser.Name);
+        HttpContext.Session.SetInt32("Session_UserId", newUser.UserId);
+        return RedirectToAction("Meetups");
+      }
+      else
+      {
+        return View("Login");
+      }
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
